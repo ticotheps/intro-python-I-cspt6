08c5da3214c3d71c0f5f197bf7774d60632b6f04
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-02-11 21:24:58 -0500

    Prints out the command line arguments in sys.argv, one per line

--- src/03_modules.py
+++ src/03_modules.py
@@ -9,7 +9,8 @@ import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
-# YOUR CODE HERE
+for (index, arg) in enumerate(sys.argv):
+    print(f"Command Line Argument #{index + 1}: {arg}")
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
