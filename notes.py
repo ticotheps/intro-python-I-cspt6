6cd5f7d2c174eee836cd9441b464e7d74718722c
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-02-11 20:58:20 -0500

    Adds content regarding list comprehensions into notes.py

--- notes.py
+++ notes.py
@@ -15,17 +15,32 @@ print('\n')
 # to change the items first in a `for` loop and then having to use the `.append()`
 # method to add it into a new list later.
 
-# Challenge: Create a new list containing the squares of all values in `numbers`
+# Challenge 1: Create a new list containing the squares of all values in `numbers`
 
 numbers = [1, 4, 9, 16, 25]
 new_numbers = []
 
-# `for` loop version of the Challenge
+# `for` loop version of the Challenge 1
 for num in numbers:
     square = num*num
     new_numbers.append(square)
 print(f"new_numbers = {new_numbers}")
 
-# `list comprehensions` version of the Challenge
+# `list comprehension` version of Challenge 1
 squares = [ num*num for num in numbers ]
-print(f"squares = {squares}")
+print(f"squares = {squares}\n")
+
+# Challenge 2: Create a new list containing only the names that start with 's'
+# so that they are properly capitalized (regardless of how the name originally
+# appears)
+
+names = ["Sarah", "jorge", "sam", "frank", "bob", "sandy", "shawn"]
+print(f"names = {names}")
+
+# `list comprehension` version of Challenge 2
+s_names = [name.capitalize() for name in names if name[0].lower() == 's']
+print(f"s_names = {s_names}\n")
+
+# Create an empty `dictionary` (very similar to an `object` in JavaScript)
+new_dict = {}
+print(f"new_dict = {new_dict}")
